ebd2e24141bf1a7dd4a2cec24f26bbba645d2288
kaha3792@colorado.edu <kaha3792@colorado.edu>
2026-08-04 13:50:57 -0600

    finished talkign about loops

--- control_flow_loop_part2.py
+++ control_flow_loop_part2.py
@@ -73,3 +73,61 @@ for (food, taste) in zip(a_list_of_food, a_list_of_tastes):
 a_list_of_colors = ["green", "red", "orange"]
 for (food, taste, color) in zip(a_list_of_food, a_list_of_tastes, a_list_of_colors):
     print(f"A {food} tastes {taste}. and is {color}")
+
+#The first thing to cover is a small utility to create lists of numbers to loop on
+#What if we were printing all sqaures between 0 and 1000:
+#So far we did something like:
+list_of_numbers = [0, 1, 2, 3, 4, 1000] #This is not ideal
+#Range = a large lsit of numbers to loop on
+for i in range(1000):
+    print(i**2) #This is a lot of numbers to print, but it works
+#It takes one argument called stop: The value at which you will stop.
+#This is the same as for i in [0,1,2,3,...,999]: print(i**2)
+#You can also give two other arguments to range: start and step
+for i in range(3, 10, 2):  
+    print(i)
+#The start, stop, and step are things we've seen before in slicing. They work the same for range 
+#Except they create an interval rather than slicing the values in an existing iterable
+for i in range(5,30,5):
+    print(i)
+
+#Now for something a little more complicated
+#Lets say we want to generate a list of all the squares of numbers 1-9
+#We do that using a for loop
+squares = []
+for i in range(1,10):
+    square = i**2
+    squares.append(square)
+print(squares)
+#We built a list one element at a time using a for loop
+#When you have to build a list (or any interable) for another list you will often encouter a LIST COMPREHENSION
+#It is simply a for loop, written in a more concise way, that builds a list
+
+squares = [i ** 2 for i in range(10)]
+#A list comprehension starts with square brackets, after all we are buidling a list
+#Then an expression comes: here, its (i ** 2) it tells us what each element of the list will be
+#Then comes the for loop, FOR STEP_VARIABLE IN ITERABLE. No colon, that's all
+
+#Another example:
+first_name = 'Katherine'
+whats_this = [x.upper() for x in first_name]
+print(whats_this)
+
+#You can add another "bell" to a list comprehension- certain elements can be filtered
+
+#We want to get the squares of all numbers between 0 and 9, but only if the sqaure is less than 30.
+small_squares = [i ** 2 for i in range (0, 10) if i ** 2 <30]
+#This is the same list comprehension as before EXCEPT there's an IF statement.
+#The IF statement conditions whether the element will be added to the list or not. If the condition is 
+#False the element is not added but if it is true than it will be added
+print(small_squares)
+
+# Let's say you have a messy folder. Your colleague is managing a messy folder
+
+folder_content = ["data.csv", "report.pdf", "summary.csv", "image.png", "notes.txt", "data2.csv", "archive.zip"] 
+#What I want: filter all elements that not .csv files
+#Reminder: You can check if a file ends with .csv by using the string method .endswith()
+#Here is a list comprehension that filters the .csv files from the folder content
+
+filtered_csv_files = [file for file in folder_content if file.endswith(".csv")]
+print(filtered_csv_files) #['data.csv', 'summary.csv', 'data2.csv']
